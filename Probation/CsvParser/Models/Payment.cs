dda21aa9f52a4354c54d63efcc531553c87ea9f9
Egorkas <37396590+Egorkas@users.noreply.github.com>
2021-08-19 23:31:07 +0300

    Add Modell Payment

---
+++ Probation/CsvParser/Models/Payment.cs
@@ -0,0 +1,18 @@
+﻿using CsvParser.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsvParser.Models
+{
+    class Payment
+    {
+        public string Name { get; set; }
+        public PaymentType Type { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public TimeSpan DeliveryTime { get; set; }
+
+    }
+}
